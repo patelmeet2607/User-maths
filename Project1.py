40b3a4475fe063f7d3d15c72418e5009f348e61d
patelmeet2607 <113482231+patelmeet2607@users.noreply.github.com>
2022-09-13 19:49:12 -0400

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+print("Hello", name + "!")
+var1 = int(input("Enter the first value: "))
+var2 = int(input("Enter the second value: "))
+res = var1 + var2
+print(res)
+res = var1 - var2
+print(res)
+res = var1 * var2
+print(res)
+res = var1/var2
+print (res)
